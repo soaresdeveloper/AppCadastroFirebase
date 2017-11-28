528347454eb494e1cc9f5ce7324d62ba6d7babfe
lucas-soares <lucas3designer@gmail.com>
2017-11-28 21:40:27 -0200

    Implementa funcionalidade de inclusao

---
+++ app/src/main/java/firebase/androidinfnet/info/appcadastrofirebase/User.java
@@ -0,0 +1,25 @@
+package firebase.androidinfnet.info.appcadastrofirebase;
+
+/**
+ * Created by Soares on 28/11/2017.
+ */
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+
+@IgnoreExtraProperties
+public class User {
+
+    public String name;
+    public String email;
+
+    // Default constructor required for calls to
+    // DataSnapshot.getValue(User.class)
+    public User() {
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
